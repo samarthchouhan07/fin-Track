dd3d41b427d42c849f6ff6878b7f2e694b307562
samarthchouhan07 <samarthchouhan29544@gmail.com>
2025-01-10 22:08:46 +0530

    account bulk delete done

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -2,27 +2,37 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
-import { columns, Payment } from "./columns";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
-
-const data:Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed52f",
-    amount: 50,
-    status: "success",
-    email: "sama@example.com",
-  },
-]
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const accountQuery=useGetAccounts()
+  const accounts=accountQuery.data || []
+  const deleteAccounts=useBulkDeleteAccounts()
+
+  const isDisabled=accountQuery.isLoading || deleteAccounts.isPending
+
+  if(accountQuery.isLoading){
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48"/>
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin"/>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -35,7 +45,10 @@ const AccountsPage = () => {
           </Button>
         </CardHeader>
         <CardContent>
-        <DataTable columns={columns} data={data} filterKey="email" onDelete={()=>{}} disabled={false}/>
+        <DataTable columns={columns} data={accounts} filterKey="email" onDelete={(row)=>{
+          const ids=row.map((r)=>r.original.id)
+          deleteAccounts.mutate({ids})
+        }} disabled={isDisabled}/>
         </CardContent>
       </Card>
     </div>
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -24,6 +24,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -40,6 +41,11 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog,confirm]=useConfirm(
+    "Are You Sure?",
+    "You are about to perform a bulk delete."
+  )
+
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -65,6 +71,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -80,6 +87,13 @@ export function DataTable<TData, TValue>({
             size={"sm"}
             variant={"outline"}
             className="ml-auto font-normal text-xs"
+            onClick={async () => {
+              const ok=await confirm()
+              if(ok){ 
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
+            }}
           >
             <Trash className="size-4 mr-2" />
             Delete ({table.getFilteredSelectedRowModel().rows.length})
